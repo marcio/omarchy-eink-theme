6f57db3d2c08173083ae67b766ec00bae5a27d0a
Marcio Gasparotto <mgasparotto@gmail.com>
2025-08-07 23:43:05 -0300

    vim config

--- neovim.lua
+++ neovim.lua
@@ -8,10 +8,10 @@ return {
 		lazy = false,
 		priority = 1000,
 		-- you can set set configuration options here
-		-- config = function()
-		--     vim.g.zenbones_darken_comments = 45
-		--     vim.cmd.colorscheme('zenbones')
-		-- end
+		config = function()
+			vim.g.zenbones_darken_comments = 45
+			vim.cmd.colorscheme('seoulbones')
+		end
 	},
 	{
 		"LazyVim/LazyVim",
