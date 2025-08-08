0519bb374cf608ea4aef6401001dd6f38d496d28
Marcio Gasparotto <mgasparotto@gmail.com>
2025-08-07 23:38:59 -0300

    changing vim default

--- neovim.lua
+++ neovim.lua
@@ -1,25 +1,17 @@
 return {
 	{
-	   "e-ink-colorscheme/e-ink.nvim",
-   		priority = 1000,
-   		config = function ()
-      		require("e-ink").setup()
-      		vim.cmd.colorscheme "e-ink"
-
-			-- choose light mode or dark mode
-			-- vim.opt.background = "dark"
-			-- vim.opt.background = "light"
-			--
-			-- or do
-			-- :set background=dark
-			-- :set background=light
-
-			local set_hl = vim.api.nvim_set_hl
-			local mono = require("e-ink.palette").mono()
-			local everforest = require("e-ink.palette").everforest()
-			set_hl(0, "Group", { fg = mono[15] })
-			set_hl(0, "Group", { fg = everforest.green })
-		end
+		"zenbones-theme/zenbones.nvim",
+		-- Optionally install Lush. Allows for more configuration or extending the colorscheme
+		-- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+		-- In Vim, compat mode is turned on as Lush only works in Neovim.
+		dependencies = "rktjmp/lush.nvim",
+		lazy = false,
+		priority = 1000,
+		-- you can set set configuration options here
+		-- config = function()
+		--     vim.g.zenbones_darken_comments = 45
+		--     vim.cmd.colorscheme('zenbones')
+		-- end
 	},
 	{
 		"LazyVim/LazyVim",
