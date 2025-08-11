d91fcaf797cf50bd37a6ac06ea4f4aea1b7a17b5
Marcio Gasparotto <mgasparotto@gmail.com>
2025-08-11 01:47:16 -0300

    nvim theme changed

--- neovim.lua
+++ neovim.lua
@@ -1,9 +1,27 @@
 return {
 	{
       'mitch1000/backpack',
-      config = function ()
-        require('backpack').setup()
-      end
+       config = function ()
+			require('backpack').setup({
+			undercurl = false,
+			commentStyle = { italic = true },
+			compile = false,
+			functionStyle = {},
+			keywordStyle = { bold = true },
+			statementStyle = { bold = true },
+			returnStyle = { italic = false, bold = true },
+			typeStyle = {},
+			transparent = false,
+			dimInactive = false,
+			terminalColors = true,
+			colors = { theme = { dark = {}, light = {} }, palette = {} },
+			contrast = "medium", -- medium, high, extreme
+			overrides = function()
+				return {}
+			end,
+			})
+			vim.cmd.colorscheme('backpack')
+  		end
     },
 	{
 		"LazyVim/LazyVim",
